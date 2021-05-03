487f9c2a75406c1503cf3644deb0fd2a5152e2e5
Ksenia Baranova <baranovaksenia1990@gmail.com>
2021-05-03 11:10:02 +0300

    FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any questions?<span>FAQ</span></h2>
+            <div className="question">
+                <h4>How do I start?</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consequatur, cupiditate deleniti
+                    dignissimos dolorum est et fuga incidunt iusto laudantium maiores maxime nam natus perspiciatis
+                    praesentium quo sequi totam, voluptatum.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="question">
+                <h4>What products do you offer?</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consequatur, cupiditate deleniti
+                    dignissimos dolorum est et fuga incidunt iusto laudantium maiores maxime nam natus perspiciatis
+                    praesentium quo sequi totam, voluptatum.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consequatur, cupiditate deleniti
+                    dignissimos dolorum est et fuga incidunt iusto laudantium maiores maxime nam natus perspiciatis
+                    praesentium quo sequi totam, voluptatum.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consequatur, cupiditate deleniti
+                    dignissimos dolorum est et fuga incidunt iusto laudantium maiores maxime nam natus perspiciatis
+                    praesentium quo sequi totam, voluptatum.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default FaqSection;
